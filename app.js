aa181328b6eaafdb7e5e9607fae70abc31f313d5
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-25 13:38:16 +0900

    各ボタンに選択肢表示

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const answers = ['スーパーファミコン', 'プレイステーション２'
 const correct = 'ニンテンドーDS'
 
 document.getElementById('js-question').textContent = question;
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
